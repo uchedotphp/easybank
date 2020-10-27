ad5b6033f3e16438ac9289130d1a3f866ad97299
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2020-10-27 14:32:24 +0100

    installed Tailwindcss into project and color spec and typography

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,700;1,300&display=swap');
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        // Primary
+        darkBlue: 'hsl(233, 26%, 24%)',
+        limeGreen: 'hsl(136, 65%, 51%)',
+        brightCyan: 'hsl(192, 70%, 51%)',
+
+        // Neutral
+        grayishBlue: 'hsl(233, 8%, 62%)',
+        lightGrayishBlue: 'hsl(220, 16%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 100%)'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
