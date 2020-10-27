39575290ce7bb191fae20fbbc35dce3b9e079cf5
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2020-10-27 16:15:33 +0100

    Finished designing the primary menu which is NavigationMenu.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <NavigationMenu />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavigationMenu from '@/components/NavigationMenu';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavigationMenu,
+  },
 }
 </script>
 
---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex justify-between pt-6 px-6 md:px-0 items-center">
+      <img src="./../assets/images/logo.svg" alt=""/>
+      <div class="capitalize text-gray-500">
+        <span class="">
+            <a class="mx-4 pb-6 border-b-4 border-transparent hover:border-limeGreen hover:text-black" href="">
+                home
+            </a>
+        </span>
+        <span class="">
+            <a class="mx-4 pb-6 border-b-4 border-transparent hover:border-limeGreen hover:text-black" href="">
+                about
+            </a>
+        </span>
+        <span class="">
+            <a class="mx-4 pb-6 border-b-4 border-transparent hover:border-limeGreen hover:text-black" href="">
+                contact
+            </a>
+        </span>
+        <span class="">
+            <a class="mx-4 pb-6 border-b-4 border-transparent hover:border-limeGreen hover:text-black" href="">
+                blog
+            </a>
+        </span>
+        <span class="">
+            <a class="mx-4 pb-6 border-b-4 border-transparent hover:border-limeGreen hover:text-black" href="">
+                careers
+            </a>
+        </span>
+      </div>
+      <button class="rounded-full capitalize bg-gradient-to-r from-limeGreen to-brightCyan py-3 px-8 text-white font-bold">
+        request invite
+      </button>
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="11">
+        <g fill="#2D314D" fill-rule="evenodd">
+          <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z" />
+        </g>
+      </svg> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationMenu",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
