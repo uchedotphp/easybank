7b83c67640f7212de1125cbcb0cb21559f733a7c
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2020-11-08 17:18:58 +0100

    still doing designs on the footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,89 +1,104 @@
 <template>
-    <div class="bg-darkBlue py-10">
-        <div class="container mx-auto flex flex-col justify-center">
-            <div class="justify-center flex">
-                <img src="./../assets/images/logoWhite.svg" alt="" srcset="">
-            </div>
-            <ul class="py-8 flex justify-center">
-                <li class="inline-block mr-4">
-                    <a href="">
-                        <img src="./../assets/images/icon-facebook.svg" alt="" srcset="">
-                    </a>
-                </li>
-                <li class="inline-block mr-4">
-                    <a href="">
-                        <img src="./../assets/images/icon-youtube.svg" alt="" srcset="">
-                    </a>
-                </li>
-                <li class="inline-block mr-4">
-                    <a href="">
-                        <img src="./../assets/images/icon-twitter.svg" alt="" srcset="">
-                    </a>
-                </li>
-                <li class="inline-block mr-4">
-                    <a href="">
-                        <img src="./../assets/images/icon-pinterest.svg" alt="" srcset="">
-                    </a>
-                </li>
-                <li class="inline-block">
-                    <a href="">
-                        <img src="./../assets/images/icon-instagram.svg" alt="" srcset="">
-                    </a>
-                </li>
-            </ul>
+  <div class="bg-darkBlue py-16">
+    <div
+      class="container mx-auto flex flex-col md:flex-row md:justify-between justify-center"
+    >
+      <div class="md:grid md:grid-cols-2">
+        <div class="">
+          <div class="justify-center flex md:justify-start">
+            <img src="./../assets/images/logoWhite.svg" alt="" srcset="" />
+          </div>
+          <ul class="py-8 md:pb-0 flex justify-center">
+            <li class="inline-block mr-4">
+              <a href="">
+                <img
+                  src="./../assets/images/icon-facebook.svg"
+                  alt=""
+                  srcset=""
+                />
+              </a>
+            </li>
+            <li class="inline-block mr-4">
+              <a href="">
+                <img
+                  src="./../assets/images/icon-youtube.svg"
+                  alt=""
+                  srcset=""
+                />
+              </a>
+            </li>
+            <li class="inline-block mr-4">
+              <a href="">
+                <img
+                  src="./../assets/images/icon-twitter.svg"
+                  alt=""
+                  srcset=""
+                />
+              </a>
+            </li>
+            <li class="inline-block mr-4">
+              <a href="">
+                <img
+                  src="./../assets/images/icon-pinterest.svg"
+                  alt=""
+                  srcset=""
+                />
+              </a>
+            </li>
+            <li class="inline-block">
+              <a href="">
+                <img
+                  src="./../assets/images/icon-instagram.svg"
+                  alt=""
+                  srcset=""
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
 
-            <ul class="capitalize text-grayishBlue text-center">
-                <li class="py-1">
-                    <a class="hover:text-limeGreen" href="">
-                        about us
-                    </a>
-                </li>
-                <li class="py-1">
-                    <a class="hover:text-limeGreen" href="">
-                        contact
-                    </a>
-                </li>
-                <li class="py-1">
-                    <a class="hover:text-limeGreen" href="">
-                        blog
-                    </a>
-                </li>
-                <li class="py-1">
-                    <a class="hover:text-limeGreen" href="">
-                        careers
-                    </a>
-                </li>
-                <li class="py-1">
-                    <a class="hover:text-limeGreen" href="">
-                        support
-                    </a>
-                </li>
-                <li class="py-1">
-                    <a class="hover:text-limeGreen" href="">
-                        privacy policy
-                    </a>
-                </li>
-            </ul>
-            <div class="flex justify-center">
-                <button
-          class="rounded-full capitalize bg-gradient-to-r from-limeGreen to-brightCyan py-3 my-6 px-8 text-white font-bold"
-        >
-          request invite
-        </button>
-            </div>
+        <ul class="capitalize text-grayishBlue text-center">
+          <li class="py-1 md:pt-0">
+            <a class="hover:text-limeGreen" href=""> about us </a>
+          </li>
+          <li class="py-1">
+            <a class="hover:text-limeGreen" href=""> contact </a>
+          </li>
+          <li class="py-1">
+            <a class="hover:text-limeGreen" href=""> blog </a>
+          </li>
+          <li class="py-1">
+            <a class="hover:text-limeGreen" href=""> careers </a>
+          </li>
+          <li class="py-1">
+            <a class="hover:text-limeGreen" href=""> support </a>
+          </li>
+          <li class="py-1">
+            <a class="hover:text-limeGreen" href=""> privacy policy </a>
+          </li>
+        </ul>
+      </div>
+      <div class="">
+        <div class="flex justify-center md:justify-end">
+          <button
+            class="rounded-full capitalize bg-gradient-to-r from-limeGreen to-brightCyan py-3 my-6 md:mt-0 px-8 text-white font-bold"
+          >
+            request invite
+          </button>
+        </div>
         <div class="capitalize text-grayishBlue py-3 text-center">
-            &#169; easybank. all rights reserved.
+          &#169; easybank. all rights reserved.
         </div>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'FooterSection'
-    }
+export default {
+  name: "FooterSection",
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
