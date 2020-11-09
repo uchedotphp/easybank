4f4a2f8ecfbf6d5cd75f0900c963b98d3885756c
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2020-11-09 15:48:55 +0100

    updated the icons of the burger menu for close

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -2,7 +2,10 @@
   <div class="container mx-auto mb-3">
     <div class="flex justify-between pt-6 px-6 md:px-0 items-center relative">
       <img src="./../assets/images/logo.svg" alt="" />
-      <div class="capitalize text-grayishBlue hidden md:block" :class="[showNav ? 'navlinks' : '']">
+      <div
+        class="capitalize text-grayishBlue hidden md:block"
+        :class="[showNav ? 'navlinks' : '']"
+      >
         <ul class="">
           <li class="inline-block">
             <a
@@ -52,7 +55,8 @@
       >
         request invite
       </button>
-      <svg @click="displayMenu"
+      <svg v-if="!showNav"
+        @click="displayMenu"
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="11"
@@ -60,6 +64,13 @@
       >
         <g fill="#2D314D" fill-rule="evenodd">
           <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z" />
+        </g>
+      </svg>
+
+      <svg @click="displayMenu" v-else xmlns="http://www.w3.org/2000/svg" width="18" height="19">
+        <g fill="#2D314D" fill-rule="evenodd">
+          <path d="M.868.661l16.97 16.97-.706.708L.162 1.369z" />
+          <path d="M.161 17.632L17.131.662l.708.706-16.97 16.97z" />
         </g>
       </svg>
     </div>
@@ -71,14 +82,14 @@ export default {
   name: "NavigationMenu",
   data() {
     return {
-      showNav: false
-    }
+      showNav: false,
+    };
   },
   methods: {
     displayMenu() {
-      this.showNav = this.showNav ? false : true
+      this.showNav = this.showNav ? false : true;
       // console.log('hi')
-    }
+    },
   },
 };
 </script>
@@ -98,7 +109,7 @@ export default {
     padding: 20px 0;
   }
 
-  .navlinks ul li{
+  .navlinks ul li {
     display: block;
     padding: 15px 0;
   }
