8f82495b8f3a31d5b98ff4cea3b6bbb747fabf72
Nwulu Uchechukwu Prince <uchedotphp@gmail.com>
2020-11-09 01:51:42 +0100

    moving forward on NavigationMenu.vue

--- src/components/JumbotronBanner.vue
+++ src/components/JumbotronBanner.vue
@@ -2,7 +2,9 @@
   <div class="mx-auto overflow-hidden md:pl-64 bg-veryLightGray pb-24 md:pb-0">
     <div class="flex flex-col md:flex-row md:justify-between md:items-center">
       <div class="md:pl-6 md:w-5/12 text-center md:text-left px-5 md:pr-0">
-        <h1 class="text-5xl md:text-6xl text-darkBlue">Next generation digital banking</h1>
+        <h1 class="text-5xl md:text-6xl text-darkBlue">
+          Next generation digital banking
+        </h1>
         <p class="text-grayishBlue text-xl md:text-2xl my-8">
           Take your financial life online. Your Easybank account will be a
           one-stop-shop for spending, saving, budgeting, investing and much
@@ -15,7 +17,7 @@
         </button>
       </div>
       <div
-        class="md:-mr-32 md:-mt-32 md:pl-32 md:bg-contain bg-no-repeat md:bg-left-top md:bg-hero-pattern order-first md:order-none -mt-32 bg-hero-pattern bg-hero bg-right-bottom bg-contain z-10"
+        class="md:-mr-32 -mt-40 md:-mt-32 md:pl-32 md:bg-contain bg-no-repeat md:bg-left-top md:bg-hero-pattern order-first md:order-none bg-hero-pattern bg-hero bg-right-bottom bg-contain z-10"
       >
         <img src="./../assets/images/image-mockups.png" alt="" srcset="" />
       </div>
@@ -32,13 +34,13 @@ export default {
 <style lang="css" scoped>
 @media only screen and (max-width: 640px) {
   .bg-hero {
-  background-image: url("./../assets/images/bg-intro-mobile.svg");
-}
+    background-image: url("./../assets/images/bg-intro-mobile.svg");
+  }
 }
 
 @media only screen and (min-width: 640px) {
-.bg-hero-pattern {
-  background-image: url("./../assets/images/bg-intro-desktop.svg");
-}
+  .bg-hero-pattern {
+    background-image: url("./../assets/images/bg-intro-desktop.svg");
+  }
 }
 </style>
--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto mb-3">
     <div class="flex justify-between pt-6 px-6 md:px-0 items-center">
       <img src="./../assets/images/logo.svg" alt=""/>
-      <div class="capitalize text-grayishBlue">
+      <div class="capitalize text-grayishBlue navlinks">
         <span class="">
             <a class="mx-4 pb-6 border-b-4 border-transparent hover:border-limeGreen hover:text-black" href="">
                 home
@@ -29,14 +29,14 @@
             </a>
         </span>
       </div>
-      <button class="rounded-full capitalize bg-gradient-to-r from-limeGreen to-brightCyan py-3 px-8 text-white font-bold">
+      <button class="hidden md:block rounded-full capitalize bg-gradient-to-r from-limeGreen to-brightCyan py-3 px-8 text-white font-bold">
         request invite
       </button>
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="11">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="11" class="md:hidden">
         <g fill="#2D314D" fill-rule="evenodd">
           <path d="M0 0h24v1H0zM0 5h24v1H0zM0 10h24v1H0z" />
         </g>
-      </svg> -->
+      </svg>
     </div>
   </div>
 </template>
